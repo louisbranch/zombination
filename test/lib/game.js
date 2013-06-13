040282f28320b1d38b0ff2475cf8448cfc1a0e8d
Luiz Branco <me@luizbranco.com>
2013-06-13 07:54:50 -0300

    Create emit alias

--- test/lib/game.js
+++ test/lib/game.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var Game = require('../../lib/game.js');
 var EventEmitter = require('events').EventEmitter;
 var zombies = require('../../lib/zombies.js');
+var sinon = require('sinon');
 
 describe('Game.new', function () {
 
@@ -11,18 +12,32 @@ describe('Game.new', function () {
     assert(game instanceof Game);
   });
 
-  it('has an Event Emitter', function () {
-    assert(game.events instanceof EventEmitter);
-  })
+  describe('.events', function () {
 
-  it('binds to infect event', function () {
-    var listeners = game.events.listeners('infect');
-    assert.deepEqual(listeners, [zombies.infect]);
+    it('is an Event Emitter', function () {
+      assert(game.events instanceof EventEmitter);
+    })
+
+    it('binds to infect event', function () {
+      var listeners = game.events.listeners('infect');
+      assert.deepEqual(listeners, [zombies.infect]);
+    });
+
+    it('binds to kill event', function () {
+      var listeners = game.events.listeners('kill');
+      assert.deepEqual(listeners, [zombies.kill]);
+    });
+
   });
 
-  it('binds to kill event', function () {
-    var listeners = game.events.listeners('kill');
-    assert.deepEqual(listeners, [zombies.kill]);
+  describe('.e', function () {
+    it('is an alias for events.emit', function () {
+      var spy = sinon.spy();
+      game.events.on('test', spy);
+      game.e('test', 1, 2, 3);
+      assert(spy.calledWith(1, 2, 3));
+    });
   });
+
 
 });
