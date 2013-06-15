7668e0e49c9e8fa00929363e822257f07c052416
Luiz Branco <me@luizbranco.com>
2013-06-15 19:38:04 -0300

    Create players cards

--- test/lib/decks.js
+++ test/lib/decks.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var decks = require('../../lib/decks.js');
+var _ = require('lodash');
 
 describe('decks', function () {
 
@@ -29,6 +30,67 @@ describe('decks', function () {
 
       it('has a group', function () {
         assert(card.group);
+      });
+
+    });
+
+  });
+
+  describe('.players', function () {
+
+    it('creates player card for each city plus special cards', function () {
+      var deck = decks.players();
+      assert.equal(deck.length, 58);
+    })
+
+    it('has a total of 5 event cards', function () {
+      var deck = decks.players();
+      var events = _.filter(deck, function (card) {
+        return card.group === 'events';
+      });
+      assert.equal(events.length, 5);
+    });
+
+    it('has a customizable difficulty level', function () {
+      var deck = decks.players('hard');
+      var events = _.filter(deck, function (card) {
+        return card.group === 'epidemics';
+      });
+      assert.equal(events.length, 6);
+    });
+
+    it('shuffles the cards each time', function () {
+      var deck1 = decks.players();
+      var deck2 = decks.players();
+      assert.notDeepEqual(deck1, deck2);
+    });
+
+    describe('cards', function () {
+      var card;
+
+      beforeEach(function () {
+        card = decks.players()[0];
+      });
+
+      it('has a name', function () {
+        assert(card.name);
+      });
+
+      it('has a group', function () {
+        assert(card.group);
+      });
+
+      describe('event cards', function () {
+
+        beforeEach(function () {
+          card = _.find(decks.players(), function (card) {
+            return card.group === 'events';
+          });
+        });
+
+        it('has a description', function () {
+          assert(card.description);
+        });
       });
 
     });
