60820f44c1a5f6a47bea13959de95dd646bd86c6
Luiz Branco <me@luizbranco.com>
2013-06-25 23:41:48 -0300

    Create initial test for epidemics

---
+++ test/lib/epidemics.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var epidemics = require('../../lib/epidemics.js');
+var Game = require('../../models/game.js');
+
+describe('epidemics', function(){
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it('returns 6 equally sized chunks of a deck', function(){
+    var chunks = epidemics.setDifficulty(game);
+    assert.equal(chunks.length, 6);
+  });
+});
